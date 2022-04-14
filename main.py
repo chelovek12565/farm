d5fb3379e0ce75f38240abe03c7211e78b8343ac
chelovek165 <bas180407@mail.ru>
2022-04-14 11:48:18 +0500

    Фронт влажности

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
+import requests
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtCore import QSize
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QPainter, QBrush, QColor
 from PyQt5 import uic
 
 
@@ -15,6 +16,37 @@ class Light(QWidget):
     def __init__(self):
         super(Light, self).__init__()
         uic.loadUi('ui/light.ui', self)
+        self.light = None
+
+
+class Humidity(QWidget):
+    def __init__(self):
+        super(Humidity, self).__init__()
+        uic.loadUi('ui/humidity.ui', self)
+        self.humidity = None
+        self.upButton_1.setIcon(QIcon('images/Arrow_Up.png'))
+        self.upButton_2.setIcon(QIcon('images/Arrow_Up.png'))
+        self.downButton_1.setIcon(QIcon('images/Arrow_Down.png'))
+        self.downButton_2.setIcon(QIcon('images/Arrow_Down.png'))
+
+
+class Temperature(QWidget):
+    def __init__(self):
+        super(Temperature, self).__init__()
+        uic.loadUi('ui/temperature.ui', self)
+
+    def paintEvent(self, paint_event):
+        painter = QPainter(self)
+        painter.setBrush(QBrush(QColor(61, 74, 255)))
+        size = self.size()
+        h = int(self.temp / 30 * 350)
+        painter.fillRect(size.width() // 2 - 30, size.height() // 2 - 175 + (350 - h), 60, h, QColor(61, 74, 255))
+        self.verticalSlider.move(size.width() // 2 + 40, size.height() // 2 - 175)
+        # painter.drawText(size.width() // 2 - 65, size.height() // 2 - 175, '30')
+        n = 30
+        for i in range(7):
+            painter.drawText(size.width() // 2 - 65, size.height() // 2 - 175 + (350 // 6) * i, str(n))
+            n -= 5
 
 
 class Main(QMainWindow):
@@ -25,20 +57,31 @@ class Main(QMainWindow):
         self.setWindowTitle('Ситифермы')
         self.scrollArea.setWidget(self.control_panel)
         self.light_view = Light()
+        self.temp_view = Temperature()
+        self.humid_view = Humidity()
         self.control_panel.lightButton.clicked.connect(self.light)
         self.control_panel.lightButton.setIcon(QIcon('images/bulb2.png'))
         self.control_panel.tempButton.setIcon(QIcon('images/temp2.png'))
         self.control_panel.humidButton.setIcon(QIcon('images/humidity.png'))
         self.control_panel.fanButton.setIcon(QIcon('images/fan.png'))
         self.control_panel.tempButton.clicked.connect(self.temp)
+        self.control_panel.humidButton.clicked.connect(self.humid)
         self.handler.addWidget(self.light_view)
+        self.handler.addWidget(self.temp_view)
+        self.handler.addWidget(self.humid_view)
+        data = requests.get('http://localhost:5000/all').json()
+        self.temp_view.temp = data['temp']
+        self.light_view.light = data['light']
         pass
 
     def light(self):
-        pass
+        self.handler.setCurrentIndex(0)
 
     def temp(self):
-        pass
+        self.handler.setCurrentIndex(1)
+
+    def humid(self):
+        self.handler.setCurrentIndex(2)
 
 
 def except_hook(cls, exception, traceback):
